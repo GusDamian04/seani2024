cb8793369d722d1bd9e407dcdaf52504e350d880
GusDamian <damiangustavoangel@gmail.com>
2024-03-14 10:44:45 -0600

    07 Implements Navigator of Candidate

--- seani/seani/settings.py
+++ seani/seani/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = [
     'library.apps.LibraryConfig',
     'career.apps.CareerConfig',
     'exam.apps.ExamConfig',
+    'home.apps.HomeConfig',
     # Django Apps
     'django.contrib.admin',
     'django.contrib.auth',
@@ -130,6 +131,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+LOGIN_REDIRECT_URL = '/exam'
+LOGOUT_REDIRECT_URL = '/'
 
 cloudinary.config(
   cloud_name = str(os.environ.get('CLOUD_NAME')),
